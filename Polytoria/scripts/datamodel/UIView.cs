484719836381074d460327bb6ea1b988885ce160
HarryBlueJay <88053351+HarryBlueJay@users.noreply.github.com>
2026-07-08 19:29:52 -0400

    Add different outline modes
    Signed-off-by: HarryBlueJay <88053351+HarryBlueJay@users.noreply.github.com>

--- Polytoria/scripts/datamodel/UIView.cs
+++ Polytoria/scripts/datamodel/UIView.cs
@@ -44,6 +44,39 @@ public partial class UIView : UIField
 			OnPropertyChanged();
 		}
 	}
+	
+	private void UpdateBorderOffset()
+	{
+		int rounded = Mathf.RoundToInt(_borderWidth);
+		if (_borderMode == BorderModeEnum.Outline) {
+			_styleBox.ExpandMarginBottom = rounded;
+			_styleBox.ExpandMarginLeft = rounded;
+			_styleBox.ExpandMarginRight = rounded;
+			_styleBox.ExpandMarginTop = rounded;
+		} else if (_borderMode == BorderModeEnum.Middle) {
+			_styleBox.ExpandMarginBottom = rounded / 2;
+			_styleBox.ExpandMarginLeft = rounded / 2;
+			_styleBox.ExpandMarginRight = rounded / 2;
+			_styleBox.ExpandMarginTop = rounded / 2;
+		} else {
+			_styleBox.ExpandMarginBottom = 0;
+			_styleBox.ExpandMarginLeft = 0;
+			_styleBox.ExpandMarginRight = 0;
+			_styleBox.ExpandMarginTop = 0;
+		}
+	}
+	
+	[Editable, ScriptProperty]
+	public BorderModeEnum BorderMode
+	{
+		get => _borderMode;
+		set
+		{
+			_borderMode = value;
+			UpdateBorderOffset();
+			OnPropertyChanged();
+		}
+	}
 
 	private void UpdateBorderOffset()
 	{
